dc95cc8cb292a3b0b0c2d4b92776c0540ba5dea4
eakathit <eakarthitx@gnail.com>
2025-10-10 15:48:34 +0700

    เริ่ม้ตน

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA1fdFsyaFlEEJKCpSU50bm78SeTrj9Ngc",
+    authDomain: "timetracker-f1e11.firebaseapp.com",
+    projectId: "timetracker-f1e11",
+    storageBucket: "timetracker-f1e11.firebasestorage.app",
+    messagingSenderId: "470557940763",
+    appId: "1:470557940763:web:6de43b02b5ba42fe46acbe",
+    measurementId: "G-C83H28RLSY"
+};
