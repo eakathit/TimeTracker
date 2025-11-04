f6c7501e260de96d4403afc43dd599d9ec25b2fb
eakathit <eakarthitx@gmail.com>
2025-11-04 13:24:15 +0700

    อัพเดทรูปไม่สำเร็จ

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,6 @@
 const firebaseConfig = {
     apiKey: "AIzaSyA1fdFsyaFlEEJKCpSU50bm78SeTrj9Ngc",
-    authDomain: "timetracker-f1e11.firebaseapp.com",
+    authDomain: "timetracker-f1e11.web.app",
     projectId: "timetracker-f1e11",
     storageBucket: "timetracker-f1e11.firebasestorage.app",
     messagingSenderId: "470557940763",
