b595ce43aa5bc756f5ec7c794a0f50e8aa7d2dee
eakathit <eakarthitx@gmail.com>
2025-11-10 16:23:11 +0700

    เเก้ไข login เเล้วรูปขึ้น

--- sw.js
+++ sw.js
@@ -2,9 +2,16 @@ self.addEventListener('fetch', (event) => {
   const url = new URL(event.request.url);
 
   // [ ★ แก้ไข ★ ]
-  // เราต้องอนุญาตให้ Request ที่ยิงไปหา Firebase (ทั้ง Functions และ Storage)
+  // เราต้องอนุญาตให้ Request ที่ยิงไปหา Firebase (ทั้ง Functions, Storage และ Auth)
   // "ทะลุ" Service Worker ไปเลย ห้ามยุ่ง
-  if (url.hostname.endsWith('cloudfunctions.net') || url.hostname.endsWith('firebasestorage.googleapis.com')) {
+  const domain = url.hostname;
+  
+  if (
+    domain.endsWith('cloudfunctions.net') ||
+    domain.endsWith('firebasestorage.googleapis.com') ||
+    domain.endsWith('timetracker-f1e11.web.app') || // [!!!] เพิ่มบรรทัดนี้
+    domain.endsWith('accounts.google.com')          // [!!!] เพิ่มบรรทัดนี้
+  ) {
     return; // ปล่อยผ่าน (Bypass)
   }
 
