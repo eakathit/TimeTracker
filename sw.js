025650718e08ca45864293cdfeec171b9ccc759c
eakathit <eakarthitx@gmail.com>
2025-11-13 14:02:39 +0700

    อัพเดท

--- sw.js
+++ sw.js
@@ -1,20 +1,16 @@
 self.addEventListener('fetch', (event) => {
   const url = new URL(event.request.url);
-
-  // [ ★ แก้ไข ★ ]
-  // เราต้องอนุญาตให้ Request ที่ยิงไปหา Firebase (ทั้ง Functions, Storage และ Auth)
-  // "ทะลุ" Service Worker ไปเลย ห้ามยุ่ง
   const domain = url.hostname;
   
   if (
+    domain === 'localhost' || // [!!!] ต้องมีบรรทัดนี้
     domain.endsWith('cloudfunctions.net') ||
     domain.endsWith('firebasestorage.googleapis.com') ||
-    domain.endsWith('timetracker-f1e11.web.app') || // [!!!] เพิ่มบรรทัดนี้
-    domain.endsWith('accounts.google.com')          // [!!!] เพิ่มบรรทัดนี้
+    domain.endsWith('timetracker-f1e11.web.app') || 
+    domain.endsWith('accounts.google.com')
   ) {
-    return; // ปล่อยผ่าน (Bypass)
+    return; // ปล่อยผ่าน
   }
 
-  // Request อื่นๆ (เช่น โหลดหน้าเว็บ, รูปภาพ) ก็ทำตามปกติ
   event.respondWith(fetch(event.request));
 });
