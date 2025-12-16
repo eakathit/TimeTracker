20255a70ec0586e473e57dc7d961fe22a6253f55
eakathit <eakarthitx@gmail.com>
2025-12-16 17:14:07 +0700

    FIXED GPS REQUEST

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "timetracker-v2.0"; // เปลี่ยนเวอร์ชั่นเมื่อมีการแก้โค้ด
+const CACHE_NAME = "timetracker-v2.1"; // เปลี่ยนเวอร์ชั่นเมื่อมีการแก้โค้ด
 const ASSETS_TO_CACHE = [
   "/",
   "/index.html",
