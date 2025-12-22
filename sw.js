a6947db8fd84c7ab49ca7054668e8f057f3cf735
eakathit <eakarthitx@gmail.com>
2025-12-22 16:04:14 +0700

    UPD FIXED UI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "timetracker-v2.2"; // เปลี่ยนเวอร์ชั่นเมื่อมีการแก้โค้ด
+const CACHE_NAME = "timetracker-v2.3"; // เปลี่ยนเวอร์ชั่นเมื่อมีการแก้โค้ด
 const ASSETS_TO_CACHE = [
   "/",
   "/index.html",
