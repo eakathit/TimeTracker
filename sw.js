a8bfa229252e800083defb3f04b3b0e62ccfb056
eakathit <eakarthitx@gmail.com>
2025-12-25 14:08:22 +0700

    UPD UI GPS

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "timetracker-v3.7"; // เปลี่ยนเวอร์ชั่นเมื่อมีการแก้โค้ด
+const CACHE_NAME = "timetracker-v3.8"; // เปลี่ยนเวอร์ชั่นเมื่อมีการแก้โค้ด
 const ASSETS_TO_CACHE = [
   "/",
   "/index.html",
